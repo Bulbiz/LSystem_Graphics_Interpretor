86b56d7ebc5d7fd6cda19f9ad36b76e17bbc04d6
Pholasa Remy <pholasa2@outlook.fr>
2020-11-03 22:32:32 +0100

    Test Version 2

--- projet/test/test_systems.ml
+++ projet/test/test_systems.ml
@@ -2,30 +2,49 @@ open OUnit2
 open Lsystems
 open Systems
 
-type symbol = A
-
-let test : symbol system = 
+type symbol = A|B|C|D 
+let test1 : symbol system = 
     {
-    axiom = Seq [Symb A];
-    rules = (function
-    | A -> Seq [Symb A;Symb A;Symb A]
+        axiom = Seq [Symb A];
+        rules = (function
+        | A -> Seq [Symb A;Symb A;Symb A]
+        | s -> Symb s
     );
 
     interp = (function
-    | A -> [Line 30])
+    | A -> [Line 30]
+    | _ -> [Turn 0])
     }
 
-let testUpdate = update test 
-let expectedUpdate = Seq [Symb A;Symb A;Symb A]
-
+let test2 : symbol system = 
+    {
+        axiom = Seq [Symb A;Symb B];
+        rules = (function
+        | A -> Seq [Symb A;Symb B]
+        | B -> Symb A
+        | s -> Symb s
+    );
+    
+    interp = (function
+    | A -> [Line 30]
+    | B -> [Turn 60]
+    | _ -> [Turn 0])
+    }    
+    
 let systems_suite =
     "SystemsTestSuite" >::: [
-        "Systems Update should return Seq[Symb A,Symb A]" >:: (fun _ ->
-            assert_equal testUpdate expectedUpdate
+        "Systems Update should return Seq[Symb A,Symb A,Symb A]" >:: (fun _ ->
+            (*test 1 for update*)
+            let testUpdate1 = update test1 in
+            let expectedUpdate1 = Seq [Symb A;Symb A;Symb A] in
+            assert_equal testUpdate1 expectedUpdate1;
         );
 
-        "Systems.f_do_nothing should print" >:: (fun _ ->
-            assert_equal "Test string" Systems.return_str
+        "Systems Update should return Seq[Symb A,Symb B,Symb A]" >:: (fun _ ->
+            (*test 2 for update*)
+            let testUpdate2 = update test2 in
+            let expectedUpdate2 = Seq [Seq[Symb A;Symb B];Symb A] in
+            assert_equal testUpdate2 expectedUpdate2
         )
     ]
 
