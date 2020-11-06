6bbfe6fa49c48b481dbaa201fd8358a681e5b947
Pholasa Remy <pholasa2@outlook.fr>
2020-11-06 09:01:34 +0100

    Petit nettoyage

--- projet/lib/systems.ml
+++ projet/lib/systems.ml
@@ -22,28 +22,17 @@ let return_0 = 0
 let return_str = "Test string"
 
 
-let update (rules:'s rewrite_rules) (word:'s word) = 
+let next_state (rules:'s rewrite_rules) (current_state:'s word) = 
   let rec update_aux (rules:'s rewrite_rules) (word:'s word)  =
     match word with
     |Symb s -> rules s
     |Branch (s) -> Branch (update_aux rules s)
-    |Seq (q) -> Seq(update_sequence rules q)(*Seq(List.map (update_aux rules) q)*)
+    |Seq (q) -> Seq(update_sequence rules q)
 
   and update_sequence (rules:'s rewrite_rules) (sequence:'s word list)  =
     match sequence with
-    | [] -> failwith "Shouldn't happen"
+    | [] -> []
     | [s] -> [update_aux rules s]
-    | x :: q -> update_aux rules x :: update_sequence rules q
+    | w :: rest -> update_aux rules w :: update_sequence rules rest
   in
-  update_aux rules word
-
-  let next_state (rules:'s rewrite_rules) (current_state:'s word) =
-    update rules current_state
-    (*
-    match rules with
-    |_->( match current_state with
-      |Symb 'A' -> Seq [Symb 'A';Symb 'A';Symb 'A']
-      |Branch(Seq [Symb 'A';Symb 'B']) -> Branch(Seq [Branch (Seq[Symb 'A';Symb 'B']);Symb 'A'])
-      | _ -> failwith ("oof") 
-    )
-    *)
+  update_aux rules current_state
