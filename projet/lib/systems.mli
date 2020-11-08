70373da215203c0a024a860aca667de87f69f540
EmileRolley <erolley@tutamail.com>
2020-11-08 12:58:57 +0100

    [FORMATTING] - Formats systems files.

--- projet/lib/systems.mli
+++ projet/lib/systems.mli
@@ -6,25 +6,21 @@ type 's word =
 
 type 's rewrite_rules = 's -> 's word
 
-type 's system = {
-  axiom : 's word;
-  rules : 's rewrite_rules;
-  interp : 's -> Turtle.command list
-}
-
-(** Put here any type and function interfaces concerning systems *)
+type 's system =
+  { axiom : 's word
+  ; rules : 's rewrite_rules
+  ; interp : 's -> Turtle.command list
+  }
 
 (** [get_current_state]
     @return the [current_state]
 *)
-val get_current_state : (unit) -> char word
+val get_current_state : unit -> char word
 
-(** [next_state rules current_state]
-    @return the next state of [current_state] according to the [rules]
+(** [next_state rules current_state] applies [rules] for each [current_state] symbols.
+    @return the resulting state.
 *)
-val next_state :('s  rewrite_rules) -> ('s word) -> ('s word)
+val next_state : 's rewrite_rules -> 's word -> 's word
 
-(** [update_state]
-    update the global current_state
-*)
-val update_state: (unit) -> (unit)
+(** [update_state] updates the global [current_state] with [next_state]. *)
+val update_state : unit -> unit
--- projet/test/test_systems.ml
+++ projet/test/test_systems.ml
@@ -3,62 +3,62 @@ open Lsystems
 open Systems
 
 let systems_suite =
-    "SystemsTestSuite" >::: [
-        "Systems.next_state should return the right word with a valid basic system." >:: (fun _ ->
-            let rules_symb : char rewrite_rules = (function
-                | 'A' -> Seq [Symb 'A';Symb 'A';Symb 'A']
-                | s -> Symb s)
-            in 
-            let axiom_symb = Symb 'A' in
-            (*test Symb for next_state*)
-            let actual_word = next_state rules_symb axiom_symb in
-            let expected_word = Seq [Symb 'A';Symb 'A';Symb 'A'] in
-            assert_equal expected_word actual_word;
-        );
-
-
-        "Systems.next_state should return the right word with a system that contains a seq inside a branch." >:: (fun _ ->
-            let rules_branch_seq = (function
-                | 'A' -> Branch (Seq[Symb 'A';Symb 'B'])
-                | 'B' -> Symb 'A'
-                | s -> Symb s) 
-            in 
-            let axiom_branch_seq = Branch(Seq [Symb 'A';Symb 'B']) in
-
-            (*test Branch and Seq for next_state*)
-            let actual_word = next_state rules_branch_seq axiom_branch_seq in
-            let expected_word = Branch(Seq [Branch (Seq[Symb 'A';Symb 'B']); Symb 'A']) in
-            assert_equal expected_word actual_word
-        );
-
-
-        "Systems.next_state should return the right word with a system that contains a branches inside a seq." >:: (fun _ ->
-            let rules_seq_branch = (function
-                | 'A' -> Branch (Seq[Symb 'A';Symb 'B'])
-                | 'B' -> Symb 'A'
-                | s -> Symb s) 
-            in 
-            let axiom_seq_branch = Seq [Branch(Seq[Symb 'A';Symb 'B']); Symb 'B'] in
-
-            (*test Branch and Seq for next_state*)
-            let actual_word = next_state rules_seq_branch axiom_seq_branch in
-            let expected_word = Seq[Branch(Seq [Branch (Seq[Symb 'A';Symb 'B']); Symb 'A']); Symb 'A'] in
-            assert_equal expected_word actual_word
-        );
-
-        "Systems.next_state should return the right word empty." >:: (fun _ ->
-            let rules_empty = (function
-                | 'A' -> Branch (Seq[Symb 'A';Symb 'B'])
-                | 'B' -> Symb 'A'
-                | s -> Symb s) 
-            in 
-            let axiom_empty = Seq [] in
-
-            (*test Empty word for next_state*)
-            let actual_word = next_state rules_empty axiom_empty in
-            let expected_word = Seq[] in
-            assert_equal expected_word actual_word
-        );
-    ]
+  "SystemsTestSuite"
+  >::: [ ("Systems.next_state should return the right word with a valid basic system."
+         >:: fun _ ->
+         let rules_symb : char rewrite_rules = function
+           | 'A' -> Seq [ Symb 'A'; Symb 'A'; Symb 'A' ]
+           | s -> Symb s
+         in
+         let axiom_symb = Symb 'A' in
+         (*test Symb for next_state*)
+         let actual_word = next_state rules_symb axiom_symb in
+         let expected_word = Seq [ Symb 'A'; Symb 'A'; Symb 'A' ] in
+         assert_equal expected_word actual_word)
+       ; ("Systems.next_state should return the right word with a system that contains a \
+           seq inside a branch."
+         >:: fun _ ->
+         let rules_branch_seq = function
+           | 'A' -> Branch (Seq [ Symb 'A'; Symb 'B' ])
+           | 'B' -> Symb 'A'
+           | s -> Symb s
+         in
+         let axiom_branch_seq = Branch (Seq [ Symb 'A'; Symb 'B' ]) in
+         (*test Branch and Seq for next_state*)
+         let actual_word = next_state rules_branch_seq axiom_branch_seq in
+         let expected_word =
+           Branch (Seq [ Branch (Seq [ Symb 'A'; Symb 'B' ]); Symb 'A' ])
+         in
+         assert_equal expected_word actual_word)
+       ; ("Systems.next_state should return the right word with a system that contains a \
+           branches inside a seq."
+         >:: fun _ ->
+         let rules_seq_branch = function
+           | 'A' -> Branch (Seq [ Symb 'A'; Symb 'B' ])
+           | 'B' -> Symb 'A'
+           | s -> Symb s
+         in
+         let axiom_seq_branch = Seq [ Branch (Seq [ Symb 'A'; Symb 'B' ]); Symb 'B' ] in
+         (*test Branch and Seq for next_state*)
+         let actual_word = next_state rules_seq_branch axiom_seq_branch in
+         let expected_word =
+           Seq
+             [ Branch (Seq [ Branch (Seq [ Symb 'A'; Symb 'B' ]); Symb 'A' ]); Symb 'A' ]
+         in
+         assert_equal expected_word actual_word)
+       ; ("Systems.next_state should return the right word empty."
+         >:: fun _ ->
+         let rules_empty = function
+           | 'A' -> Branch (Seq [ Symb 'A'; Symb 'B' ])
+           | 'B' -> Symb 'A'
+           | s -> Symb s
+         in
+         let axiom_empty = Seq [] in
+         (*test Empty word for next_state*)
+         let actual_word = next_state rules_empty axiom_empty in
+         let expected_word = Seq [] in
+         assert_equal expected_word actual_word)
+       ]
+;;
 
 let () = run_test_tt_main systems_suite
